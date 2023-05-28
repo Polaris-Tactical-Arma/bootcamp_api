e640240900f694591b287c99d45b1db7c72329f6
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2023-05-28 18:25:51 +0200

    update

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from pb import client
 
 
-
 collection = client.collection("bootcamp")
 app = Flask(__name__)
 
@@ -10,41 +9,43 @@ app = Flask(__name__)
 @app.route("/bootcamp", methods=["POST"])
 def index():
     data = request.get_json()
-    print(data)
-
     uid = data["playerUID"]
     record = collection.get_list(1, 1, {"filter": f"playerUID = {uid}"})
 
     if not record.items:
         collection.create(data)
+        return jsonify({"message": "OK"})
 
-        return {"message": "OK"}
-
-    item = record.items[0]
-
-    if item.data is None:
-        item.data = {}
+    item = record.items[0].collection_id
+    item["data"] = item.get("data") if item.get("data") is not None else {}
 
     section = list(data["data"].keys())[0]
+    item["data"][section] = item["data"].get(section, {})
 
-    if section not in item.data:
-        item.data[section] = {}
+    update_data(item["data"][section], data["data"][section])
 
-    for key, value in data["data"][section].items():
-        if key in item.data[section] and isinstance(value, list):
-            item.data[section][key].extend(value)
-        else:
-            item.data[section][key] = value
+    # If player name is empty in the existing item, use player name from the request data
+    if not item["playerName"]:
+        item["playerName"] = data["playerName"]
+
     updated_item_data = {
-        "playerUID": item.player_uid,
-        "playerName": item.player_name,
-        "discordUserId": item.discord_user_id,
-        "discordChannelId": item.discord_channel_id,
-        "data": item.data,
+        "playerUID": str(item["playerUID"]),
+        "playerName": item["playerName"],
+        "discordUserId": item["discordUserId"],
+        "discordChannelId": item["discordChannelId"],
+        "data": item["data"],
     }
-    collection.update(item.id, updated_item_data)
 
-    return {"message": "OK"}
+    collection.update(item["id"], updated_item_data)
+    return jsonify({"message": "OK"})
+
+
+def update_data(old_data, new_data):
+    for key, value in new_data.items():
+        if key in old_data and isinstance(value, list):
+            old_data[key].extend(value)
+        else:
+            old_data[key] = value
 
 
 @app.route("/bootcamp/<playerUID>", methods=["GET"])
@@ -54,18 +55,18 @@ def get_player_data(playerUID):
     if not record.items:
         return {"message": "Player not found"}, 404
 
-    item = record.items[0]
-    data = jsonify(
+    collection_item = record.items[0]
+    item = collection_item.collection_id
+
+    return jsonify(
         {
-            "playerUID": str(item.player_uid),
-            "playerName": item.player_name,
-            "discordUserId": item.discord_user_id,
-            "discordChannelId": item.discord_channel_id,
-            "data": item.data,
+            "playerUID": str(item["playerUID"]),
+            "playerName": item["playerName"],
+            "discordUserId": item["discordUserId"],
+            "discordChannelId": item["discordChannelId"],
+            "data": item["data"],
         }
     )
-
-    return data
 
 
 if __name__ == "__main__":
